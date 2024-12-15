498b8458fac02a9dcedf87e6fe38fda5c3a07f53
qloha <qloha@users.noreply.github.com>
2024-12-15 11:48:00 -0800

    wahhh

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url'; // Import fileURLToPath from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
     base: '/SmoothMath-Docs/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                notFound: resolve(__dirname, '404.html'),
+            },
+        },
+    },
 });
